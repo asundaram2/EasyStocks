bb9a988cc254a50a0b7348bf19552bff91de665d
Arun Sundaram <arunks2@illinois.edu>
2018-12-11 00:06:39 -0600

    API kinda works

--- app/src/main/java/cs125/arunmatthias/easystocks/DisplayStockInfo.java
+++ app/src/main/java/cs125/arunmatthias/easystocks/DisplayStockInfo.java
@@ -9,64 +9,59 @@ import android.view.Display;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * displays the name of the stock when you click on the list item
  */
 public class DisplayStockInfo extends AppCompatActivity {
+
+    public static TextView displayAPIText;
+    public URL url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_stock_info);
 
-        // displays the graph of the stock using the position and key from ThirdActivity.class
-        //TODO stock info API here
-
-        Intent intent = getIntent();
-        int index = intent.getIntExtra("ITEM_INDEX", -1);
-
-        if (index > -1) {
-            int graph = getGraph(index);
-            ImageView imageview = (ImageView) findViewById(R.id.imageView);
-            scaleGraph(imageview, graph);
+        // displays the name of the stock using the position and key from ThirdActivity.class
+        if (getIntent().hasExtra("ITEM_INDEX")) {
+            displayAPIText = (TextView) findViewById(R.id.displayAPI);
+            String text = getIntent().getExtras().getString("ITEM_INDEX");
+            if (text.equals("AAPL")) {
+                fetchData process = null;
+                try {
+                    process = new fetchData(new URL("https://api.iextrading.com/1.0/stock/aapl/earnings"));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                process.execute();
+            } else if (text.equals("GOOGL")) {
+                fetchData process = null;
+                try {
+                    process = new fetchData(new URL("https://api.iextrading.com/1.0/stock/googl/earnings"));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                process.execute();
+            } else if(text.equals("FB")) {
+                fetchData process = null;
+                try {
+                    process = new fetchData(new URL("https://api.iextrading.com/1.0/stock/fb/earnings"));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                process.execute();
+            } else {
+                displayAPIText.setText(text);
+            }
         }
     }
 
-    private int getGraph(int index) {
-        switch (index) {
-            case 0:
-                return R.drawable.apple;
-            case 1:
-                return R.drawable.google;
-            case 2:
-                return R.drawable.tyson;
-            case 3:
-                return R.drawable.dfs;
-            case 4:
-                return R.drawable.t;
-            default:
-                return R.drawable.apple;
-        }
+    public URL getURL() {
+        return url;
     }
-
-    private void scaleGraph(ImageView graph, int picture) {
-
-        Display screen = getWindowManager().getDefaultDisplay();
-        BitmapFactory.Options options = new BitmapFactory.Options();
-
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(getResources(), picture, options);
-
-        int width = options.outWidth;
-        int screenWidth = screen.getWidth();
-        if (width > screenWidth) {
-            int ratio = Math.round((float)width / (float)screenWidth);
-            options.inSampleSize = ratio;
-        }
-
-        options.inJustDecodeBounds = false;
-        Bitmap scaledGraph = BitmapFactory.decodeResource(getResources(), picture, options);
-        graph.setImageBitmap(scaledGraph);
-    }
-
 }
---
+++ app/src/main/java/cs125/arunmatthias/easystocks/fetchData.java
@@ -0,0 +1,50 @@
+package cs125.arunmatthias.easystocks;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class fetchData extends AsyncTask<Void, Void, Void> {
+
+    URL url;
+    fetchData(URL setURL) {
+        url = setURL;
+    }
+
+    String data = "";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = this.url;
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null) {
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        DisplayStockInfo.displayAPIText.setText(data);
+    }
+}
